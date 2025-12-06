935330e3ffc8d181d49e0c8d62f36a9522cb5563
Farah Finn <43724242+farahfinn@users.noreply.github.com>
2025-12-05 20:35:20 -0500

    added delete method to the EmbeddedStruct

--- target/debug/deps/tiny_db_exp-2365338fdae78cc8.d
+++ target/debug/deps/tiny_db_exp-2365338fdae78cc8.d
@@ -1,9 +1,12 @@
-/home/farah/learning/rust/lgr/capstone/tiny-db-exp/target/debug/deps/tiny_db_exp-2365338fdae78cc8.d: src/lib.rs src/datastore/mod.rs Cargo.toml
+/home/farah/learning/rust/lgr/capstone/tiny-db-exp/target/debug/deps/tiny_db_exp-2365338fdae78cc8.d: src/lib.rs src/datastore/mod.rs src/datastore/database.rs src/datastore/error.rs src/datastore/record.rs Cargo.toml
 
-/home/farah/learning/rust/lgr/capstone/tiny-db-exp/target/debug/deps/libtiny_db_exp-2365338fdae78cc8.rmeta: src/lib.rs src/datastore/mod.rs Cargo.toml
+/home/farah/learning/rust/lgr/capstone/tiny-db-exp/target/debug/deps/libtiny_db_exp-2365338fdae78cc8.rmeta: src/lib.rs src/datastore/mod.rs src/datastore/database.rs src/datastore/error.rs src/datastore/record.rs Cargo.toml
 
 src/lib.rs:
 src/datastore/mod.rs:
+src/datastore/database.rs:
+src/datastore/error.rs:
+src/datastore/record.rs:
 Cargo.toml:
 
 # env-dep:CLIPPY_ARGS=
